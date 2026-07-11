9400052bac4a0c45e948d4b17bbfa2d34b7944da
Ahmed Awad <ahmeda6777602@gmail.com>
2026-07-11 11:09:23 +0300

    Update code to ready publish

--- Mods/always-on-top.wh.cpp
+++ Mods/always-on-top.wh.cpp
@@ -3,7 +3,7 @@
 // @name       Always On Top Windows
 // @description Pin/unpin active window with Ctrl+Shift+T (with speech notifications)
 // @version    1.0
-// @author     Ahmed_Awad_7
+// @author     AhmedAwad7
 // @github     https://github.com/AhmedAwad7
 // @license    MIT
 // @include    *
@@ -12,7 +12,55 @@
 
 // ==WindhawkModReadme==
 /*
-readme.txt
+Always On Top Windows
+Windhawk Mod
+==========================
+
+![Screenrecord](https://raw.githubusercontent.com/AhmedAwad7/Always-On-Top-Windows-Windhawk-Mod-/main/Screenrecord/gif.gif)
+
+لتفعيل تثبيت\إلغاء التثبيت فوق النوافذ الأخري أستخدم أختصار `Ctrl+Shift+T`
+
+من مميزات هذه الإضافة أنها ستساعدك في تعدد المهام
+
+لأن هدف هذا المود هو تثبيت النافذة فوق التطبيقات الأخري
+
+نرحب بالمساهمين سواء كان لديك اقتراح Issues أو التعديل على الكود Pull requests على Github
+
+يعتمد هذا المود على تقنية Ease of Access Center لنطق إذا كان التثبيت يعمل أم لا 
+
+إذا كان هناك مشكلة في هذه التقنية داخل الويندوز فأحتمال أنه لا يصدر صوت كلام عند الضغط على الأختصار
+
+**متطلبات المود**
+
+Windhawk ولكن لا يكون قديم جداً
+- ويندوز 7 --> يعمل بكفائة
+- ويندوز 8 --> يعمل بكفائة
+- ويندوز 8.1 --> يعمل بكفائة
+- ويندوز 10\11 --> لم أجربه (إذا كان يعمل بكفاءة معك فأخبرني في Issues على Github)
+---
+Always On Top Windows
+Windhawk Mod
+==========================
+
+To enable/disable staying on top of other windows, use the shortcut `Ctrl+Shift+T`
+
+One of the features of this addition is that it will help you multitask
+
+Because the goal of this mod is to pin the window on top of other applications
+
+We welcome contributors, whether you have suggestions (Issues) or code modifications (Pull requests) on Github
+
+This mod relies on the Ease of Access Center technology to announce whether the pin is active or not
+
+If there is an issue with this technology in Windows, it's possible that no speech will be issued when pressing the shortcut
+
+**Mod Requirements**
+
+Windhawk, but not too old
+- Windows 7 --> Works efficiently
+- Windows 8 --> Works efficiently
+- Windows 8.1 --> Works efficiently
+- Windows 10/11 --> I haven't tried it (if it works well for you, please let me know in Issues on Github)
 */
 // ==/WindhawkModReadme==
 
